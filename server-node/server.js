311882859e8af9aeb3935f1ad7cf20aa7c295749
lauri naukkarinen <gettuget@gmail.com>
2014-01-26 14:12:58 +0200

    Adding node socket.io server for client

--- server-node/server.js
+++ server-node/server.js
@@ -4,9 +4,8 @@ var express = require('express');
 var app = express();
 var server = http.createServer(app);
 
-server.listen(3000, function () {
-  console.log('3000');
-});
+app.use(express.static(__dirname + '/../client'));
+app.listen(8086, function () { });
 
 var io = require('socket.io').listen(server);
 
